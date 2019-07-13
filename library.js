6a06d5e685a2513f7bb60d95e80ddc1228a872ac
avu916 <andrewvu.cs@gmail.com>
2019-07-13 13:16:53 -0700

    retrieving text

--- library.js
+++ library.js
@@ -16,14 +16,25 @@ let driver = new webdriver.Builder()
 driver.get("https://library-app.firebaseapp.com/");
 
 driver.findElement(By.css('input'));
-driver.findElement(By.css('btn-lg')).getText()
+driver.findElement(By.css('.btn-lg')).getText()
     .then(txt => {
         console.log("The text of the button is: " + txt );
     });
-driver.findElements(By.css('nav li'));
 
-driver.sleep(10000);
-// driver.quit();
-setTimeout(() => {
-    driver.quit();
-}, 100);
+//Find the elements, grabs them all in the array, then map each single element getText
+driver.findElements(By.css('nav li'))
+    .then(elements => {
+        elements.map(el => {
+            el.getText()
+                .then(txt => {
+                    console.log("the text of the navbar element is: " + txt);
+                });
+        });
+    });
+
+// driver.sleep(10000);
+// // driver.quit();
+// setTimeout(() => {
+//     driver.quit();
+// }, 100);
+// await driver.quit();
